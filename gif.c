e05da0293245c5cec69e69ab136c885fec57937f
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-12-23 03:29:24 +0530

    guys... we might've actually nailed it!!!! yay!!!

--- gif.c
+++ gif.c
@@ -58,53 +58,94 @@ size_t extract_gif_frames(const char *fname, uint8_t ***frames,
     return 0;
   }
 
+  int cell_w = handler->width / MATRIX_WIDTH;
+  int cell_h = handler->height / MATRIX_HEIGHT;
+
   size_t current_frame = 0;
   while (gd_get_frame(handler) != 0) {
 
     int delay_in_ms = handler->gce.delay * 10;
 
-    uint8_t *frame_buffer = (uint8_t *)malloc(NUM_LEDS * BYTES_PER_LED);
-    if (!frame_buffer) {
+    uint8_t *frame_buffer =
+        (uint8_t *)malloc(handler->width * handler->height * BYTES_PER_LED);
+
+    uint8_t *data_buffer = (uint8_t *)malloc(NUM_LEDS * BYTES_PER_LED);
+
+    if (!frame_buffer || !data_buffer) {
       free(*frames);
       free(*delays_in_ms);
       gd_close_gif(handler);
-      fprintf(stderr, "framebuffer allocation failed\n");
+      fprintf(stderr, "framebuffer or databuffer allocation failed\n");
       return 0;
     }
 
     gd_render_frame(handler, frame_buffer);
 
-    for (int i = 0; i < NUM_LEDS; i++) {
+    for (int y = 0; y < MATRIX_HEIGHT; y += 1) {
+      for (int x = 0; x < MATRIX_WIDTH; x += 1) {
+        int px = x * cell_w + cell_w / 2;
+        int py = y * cell_h + cell_h / 2;
+        size_t index = py * handler->width + px;
 
-      uint8_t r = frame_buffer[i * 3 + 0];
-      uint8_t g = frame_buffer[i * 3 + 1];
-      uint8_t b = frame_buffer[i * 3 + 2];
+        uint8_t r = frame_buffer[index * 3 + 0];
+        uint8_t g = frame_buffer[index * 3 + 1];
+        uint8_t b = frame_buffer[index * 3 + 2];
 
-      // color correction
-      r = clamp_u8((int)(r * R_CORRECTION));
-      g = clamp_u8((int)(g * G_CORRECTION));
-      b = clamp_u8((int)(b * B_CORRECTION));
+        // color correction
+        r = clamp_u8((int)(r * R_CORRECTION));
+        g = clamp_u8((int)(g * G_CORRECTION));
+        b = clamp_u8((int)(b * B_CORRECTION));
 
-      // brightness
-      r = (uint8_t)(r * br);
-      g = (uint8_t)(g * br);
-      b = (uint8_t)(b * br);
+        // brightness
+        r = (uint8_t)(r * br);
+        g = (uint8_t)(g * br);
+        b = (uint8_t)(b * br);
 
-      // gamma correction
-      r = gamma_lut_r[clamp_u8(r)];
-      g = gamma_lut_g[clamp_u8(g)];
-      b = gamma_lut_b[clamp_u8(b)];
+        // gamma correction
+        r = gamma_lut_r[clamp_u8(r)];
+        g = gamma_lut_g[clamp_u8(g)];
+        b = gamma_lut_b[clamp_u8(b)];
 
-      // update frame buffer
-      frame_buffer[i * 3 + 0] = r;
-      frame_buffer[i * 3 + 1] = g;
-      frame_buffer[i * 3 + 2] = b;
+        // update frame buffer
+        size_t data_index = y * MATRIX_WIDTH + x;
+        data_buffer[data_index * 3 + 0] = r;
+        data_buffer[data_index * 3 + 1] = g;
+        data_buffer[data_index * 3 + 2] = b;
+      }
     }
 
-    (*frames)[current_frame] = frame_buffer;
+    // for (int i = 0; i < NUM_LEDS; i++) {
+    //
+    //   uint8_t r = frame_buffer[i * 3 + 0];
+    //   uint8_t g = frame_buffer[i * 3 + 1];
+    //   uint8_t b = frame_buffer[i * 3 + 2];
+    //
+    //   // color correction
+    //   r = clamp_u8((int)(r * R_CORRECTION));
+    //   g = clamp_u8((int)(g * G_CORRECTION));
+    //   b = clamp_u8((int)(b * B_CORRECTION));
+    //
+    //   // brightness
+    //   r = (uint8_t)(r * br);
+    //   g = (uint8_t)(g * br);
+    //   b = (uint8_t)(b * br);
+    //
+    //   // gamma correction
+    //   r = gamma_lut_r[clamp_u8(r)];
+    //   g = gamma_lut_g[clamp_u8(g)];
+    //   b = gamma_lut_b[clamp_u8(b)];
+    //
+    //   // update frame buffer
+    //   frame_buffer[i * 3 + 0] = r;
+    //   frame_buffer[i * 3 + 1] = g;
+    //   frame_buffer[i * 3 + 2] = b;
+    // }
+    //
+    (*frames)[current_frame] = data_buffer;
     (*delays_in_ms)[current_frame] =
         delay_in_ms <= 0 ? MIN_DELAY_IN_MS : delay_in_ms;
     current_frame += 1;
+    free(frame_buffer);
   }
 
   gd_close_gif(handler);
