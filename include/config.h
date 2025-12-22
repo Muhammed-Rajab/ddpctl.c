2299b06b29075ab7fa5a36294ac031353ac1dfe6
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-12-23 05:29:37 +0530

    cleaned config.h

--- include/config.h
+++ include/config.h
@@ -5,19 +5,17 @@
 #define MATRIX_WIDTH 16
 #define MATRIX_HEIGHT 16
 
+// WARN: don't change these values
 #define BYTES_PER_LED 3
 #define NUM_LEDS (MATRIX_WIDTH * MATRIX_HEIGHT)
 
-// gamma correction per channel
+// gamma correction per channel. tune these values according to
+// your LED's response.
 #define R_GAMMA 2.2f
 #define G_GAMMA 2.0f
 #define B_GAMMA 2.4f
 
-// color correction per channel
-// #define R_CORRECTION 1.00f
-// #define G_CORRECTION 0.82f
-// #define B_CORRECTION 0.70f
-
+// color correction per channel (for me 1.00f is good enough)
 #define R_CORRECTION 1.00f
 #define G_CORRECTION 1.00f
 #define B_CORRECTION 1.00f
